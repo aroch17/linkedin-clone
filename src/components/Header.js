399848b25779b5638257f70ea1f5d2dfdd072f7b
aroch17 <ayushrochlani0405@gmail.com>
2023-03-31 00:50:34 +0100

    Added responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './styles/Header.css'
-import SearchIcon from '@mui/icons-material/Search';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import HomeIcon from '@mui/icons-material/Home';
 import SuperVisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HeaderOption from './HeaderOption';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectUser } from '../features/userSlice';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { Navbar, Container, Nav, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
     const user = useSelector(selectUser)
@@ -23,30 +23,34 @@ function Header() {
         signOut(auth)
     }
     return (
-        <div className='header'>
-            <div className='header__left'>
-                <div className='linkedin__icon'>
-                    <LinkedInIcon fontSize='large' />
-                </div>
-
-                <div className='header__search'>
-                    {/* Icon */}
-                    <SearchIcon />
-                    <input type='text' placeholder='Search' />
-                </div>
-            </div>
-
-            <div className='header__right'>
-                <HeaderOption Icon={HomeIcon} title='Home' />
-                <HeaderOption Icon={SuperVisorAccountIcon} title='My Network' />
-                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
-                <HeaderOption Icon={ChatIcon} title='Messaging' />
-                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
-                <div className="accountIcon">
-                    <HeaderOption isAvatar={true} avatarSrc={user?.profilePic} title={user ? 'Logout' : 'My Account'} onClick={logoutOfApp} />
-                </div>
-            </div>
-        </div>
+        <Navbar className='header' bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#"><LinkedInIcon fontSize='large' /></Navbar.Brand>
+                <Form className="d-flex">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                </Form>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href=""><HeaderOption Icon={HomeIcon} title='Home' /></Nav.Link>
+                        <Nav.Link href=""><HeaderOption Icon={SuperVisorAccountIcon} title='My Network' /></Nav.Link>
+                        <Nav.Link href=""><HeaderOption Icon={BusinessCenterIcon} title='Jobs' /></Nav.Link>
+                        <Nav.Link href=""><HeaderOption Icon={ChatIcon} title='Messaging' /></Nav.Link>
+                        <Nav.Link href=""><HeaderOption Icon={NotificationsIcon} title='Notifications' /></Nav.Link>
+                        <Nav.Link>
+                            <div className="accountIcon">
+                                <HeaderOption isAvatar={true} avatarSrc={user?.profilePic} title={user ? 'Logout' : 'My Account'} onClick={logoutOfApp} />
+                            </div>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
