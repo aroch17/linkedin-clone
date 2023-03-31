f0101cb105ebe5dc062f20a9a2514d1c71a5e1d2
aroch17 <ayushrochlani0405@gmail.com>
2023-03-31 01:19:21 +0100

    Centred header

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,34 +23,36 @@ function Header() {
         signOut(auth)
     }
     return (
-        <Navbar className='header' bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#"><LinkedInIcon fontSize='large' /></Navbar.Brand>
-                <Form className="d-flex">
-                    <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                    />
-                </Form>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href=""><HeaderOption Icon={HomeIcon} title='Home' /></Nav.Link>
-                        <Nav.Link href=""><HeaderOption Icon={SuperVisorAccountIcon} title='My Network' /></Nav.Link>
-                        <Nav.Link href=""><HeaderOption Icon={BusinessCenterIcon} title='Jobs' /></Nav.Link>
-                        <Nav.Link href=""><HeaderOption Icon={ChatIcon} title='Messaging' /></Nav.Link>
-                        <Nav.Link href=""><HeaderOption Icon={NotificationsIcon} title='Notifications' /></Nav.Link>
-                        <Nav.Link>
-                            <div className="accountIcon">
-                                <HeaderOption isAvatar={true} avatarSrc={user?.profilePic} title={user ? 'Logout' : 'My Account'} onClick={logoutOfApp} />
-                            </div>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="mainHeader">
+            <Navbar className='header' bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#"><LinkedInIcon fontSize='large' /></Navbar.Brand>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                    </Form>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href=""><HeaderOption Icon={HomeIcon} title='Home' /></Nav.Link>
+                            <Nav.Link href=""><HeaderOption Icon={SuperVisorAccountIcon} title='My Network' /></Nav.Link>
+                            <Nav.Link href=""><HeaderOption Icon={BusinessCenterIcon} title='Jobs' /></Nav.Link>
+                            <Nav.Link href=""><HeaderOption Icon={ChatIcon} title='Messaging' /></Nav.Link>
+                            <Nav.Link href=""><HeaderOption Icon={NotificationsIcon} title='Notifications' /></Nav.Link>
+                            <Nav.Link>
+                                <div className="accountIcon">
+                                    <HeaderOption isAvatar={true} avatarSrc={user?.profilePic} title={user ? 'Logout' : 'My Account'} onClick={logoutOfApp} />
+                                </div>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     )
 }
 
